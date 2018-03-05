a1d9e1fa54fd305853de1c23b3ee6f9e10aa26fb
Unknown <alexander.braumann@tuwien.ac.at>
2018-03-05 22:24:13 +0100

    Created test-directory
    Here test scripts can be put.

---
+++ tests/consistency.R
@@ -0,0 +1,20 @@
+library(devtools)
+load_all(pkg="./MFtimeSeries")
+
+## generate a model
+
+m <- modgen(n=2, p=1)
+
+## or set-up your own model
+
+
+## run Monte Carlo simulation with large T values
+
+
+
+
+
+## and report the Frob.-norms to the true values
+
+
+
